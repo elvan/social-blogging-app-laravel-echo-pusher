90b5da3854e537b5f662c8ca21f7776bad7196dc
Elvan Hidayat <elvanhd@gmail.com>
2023-03-04 01:08:20 +0700

    logging users out

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,12 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function logout()
+    {
+        auth()->logout();
+        return redirect('/')->with('success', 'You are now logged out.');
+    }
+
     public function showCorrectHomepage()
     {
         if (auth()->check()) {
@@ -27,9 +33,9 @@ class UserController extends Controller
 
         if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
             $request->session()->regenerate();
-            return 'Congrats!!!';
+            return redirect('/')->with('success', 'You have successfully logged in.');
         } else {
-            return 'Sorry!!!';
+            return redirect('/')->with('failure', 'Invalid login.');
         }
     }
 
@@ -43,7 +49,8 @@ class UserController extends Controller
 
         $incomingFields['password'] = bcrypt($incomingFields['password']);
 
-        User::create($incomingFields);
-        return 'Hello from register function';
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/')->with('success', 'Thank you for creating an account.');
     }
 }
