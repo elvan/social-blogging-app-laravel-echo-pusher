50f9c5b9031c1ce63d4f1a0a4deb14fddc819f24
Elvan Hidayat <elvanhd@gmail.com>
2023-03-04 13:37:02 +0700

    resizing images

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,14 +5,24 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rule;
+use Intervention\Image\Facades\Image;
 
 class UserController extends Controller
 {
     public function storeAvatar(Request $request)
     {
-        $request->file('avatar')->store('public/avatars');
-        return 'hey';
+        $request->validate([
+            'avatar' => 'required|image|max:3000'
+        ]);
+
+        $user = auth()->user();
+
+        $filename = $user->id . '-' . uniqid() . '.jpg';
+
+        $imgData = Image::make($request->file('avatar'))->fit(120)->encode('jpg');
+        Storage::put('public/avatars/' . $filename, $imgData);
     }
 
     public function showAvatarForm()
