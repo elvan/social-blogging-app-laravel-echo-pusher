63eb34efefcac09ec0b1b92f821a3695596c1c80
Elvan Hidayat <elvanhd@gmail.com>
2023-03-04 07:48:22 +0700

    displaying single blog post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,9 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'body', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
