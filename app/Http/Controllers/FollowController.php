f0ec51ba558148f3514c5f312f93426778917dc7
Elvan Hidayat <elvanhd@gmail.com>
2023-03-04 18:17:29 +0700

    removing a follow

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -16,7 +16,10 @@ class FollowController extends Controller
         }
 
         // you cannot follow someone you're already following
-        $existCheck = Follow::where([['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id]])->count();
+        $existCheck = Follow::where([
+            ['user_id', '=', auth()->user()->id],
+            ['followeduser', '=', $user->id]
+        ])->count();
 
         if ($existCheck) {
             return back()->with('failure', 'You are already following that user.');
@@ -30,7 +33,12 @@ class FollowController extends Controller
         return back()->with('success', 'User successfully followed.');
     }
 
-    public function removeFollow()
+    public function removeFollow(User $user)
     {
+        Follow::where([
+            ['user_id', '=', auth()->user()->id],
+            ['followeduser', '=', $user->id]
+        ])->delete();
+        return back()->with('success', 'User succesfully unfollowed.');
     }
 }
